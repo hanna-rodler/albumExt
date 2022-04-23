2a9c1e2cf65149568f826bd2555b3b7045cec11d
hanna-rodler <haj.rodler@gmail.com>
2022-04-23 19:18:36 +0200

    added GenreRepo

---
+++ packages/albums/Classes/Domain/Repository/GenreRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HannaRodler\Albums\Domain\Repository;
+
+use HannaRodler\Albums\Domain\Model\Dto\Filter;
+
+/**
+ * This file is part of the "Albums &amp; Songs" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2022 Hanna Rodler
+ */
+
+/**
+ * The repository for Genres
+ */
+class GenreRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+  public function findByFilter(Filter $filter){
+    $query = $this->createQuery();
+    $query->getQuerySettings()->setRespectStoragePage(false);
+    
+    $constraints=[];
+    if($filter->getGenres()){
+      $constraints[]=$query->equals('genres', $filter->getGenres());
+    }
+    
+    
+    return $query->execute();
+  }
+}
